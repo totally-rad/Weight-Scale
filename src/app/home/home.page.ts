ac41da27a7147c77bcd926f98b0e4c603a528f7c
Raddy <rx3mer@live.co.uk>
2019-02-05 16:36:21 +0000

    added some css

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor() {}
+
+  // scrollNum = document.getElementById("myDial");  
+  // scrollNum.addEventListener('scroll', functionName);
+
+
+  ngOnInit() {
+
+  }
+
+  logScrollStart(){
+    console.log("sda");
+  }
+
+  logScrolling(){
+    console.log("sda");
+  }
+
+  logScrollEnd(){
+    console.log("sda");
+  }
+
+  
+
+  // var content = document.querySelector('ion-content');
+  // content.scrollEvents = true;
+  // content.addEventListener('ionScrollStart', () => console.log('scroll start'));
+  // content.addEventListener('ionScroll', (ev) => console.log('scroll', ev.detail));
+  // content.addEventListener('ionScrollEnd', () => console.log('scroll end'));
 
 }
+
+
+
+
